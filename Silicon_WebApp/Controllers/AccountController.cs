2e39309ff82bffe9fe24bd61133f37352d5922b8
Markusloka <Markuslokander97@gmail.com>
2024-04-15 23:53:52 +0200

    finsihed with all auth, started with account

---
+++ Silicon_WebApp/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_WebApp.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Details()
+    {
+        return View();
+    }
+}
