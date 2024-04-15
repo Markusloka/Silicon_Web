642ec8f70fd5eb842b9f7e439c73a62b4129c485
Markusloka <Markuslokander97@gmail.com>
2024-04-15 23:34:09 +0200

    started with auth, soon done

---
+++ Silicon_WebApp/ViewModels/SignInViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Silicon_WebApp.ViewModels;
+
+public class SignInViewModel
+{
+    [Required]
+    [Display(Name = "E-mail address", Prompt ="Enter your e-mail address")]
+    [DataType(DataType.EmailAddress)]
+
+
+
+
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [Display(Name = "Password", Prompt = "Enter your password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "RemeberMe", Prompt = "Remember me")]
+
+    public bool IsPresistent {  get; set; }
+}
---
+++ Silicon_WebApp/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Silicon_WebApp.ViewModels;
+
+public class SignUpViewModel
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+    public bool TermsAndConditions { get; set; } 
+}
