94213746626ed569282c7456b76cdecc9b2355c7
Markusloka <Markuslokander97@gmail.com>
2024-04-15 22:26:41 +0200

    added database

---
+++ Silicon_WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_WebApp.Controllers;
+
+public class DefaultController : Controller
+{
+    public IActionResult Home()
+    {
+        return View();
+    }
+}
